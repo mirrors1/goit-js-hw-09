ac99a655f2b9251ed7d8abb6dcba816ac9cdb0f6
mirrors1 <sasha195@i.ua>
2024-11-17 18:58:17 +0000

    Deploying to gh-pages from @ mirrors1/goit-js-hw-09@6b1a088e824d727decf17449330e1e59751ddc2e 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/styles-B4s-4mBC.js";console.log("I am 1-gallery.js");
+//# sourceMappingURL=1-gallery.js.map
---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/styles-B4s-4mBC.js";console.log("I am 2-form.js");
+//# sourceMappingURL=2-form.js.map
